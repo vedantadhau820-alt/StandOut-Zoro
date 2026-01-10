46dc3dda0193fbd7cfcc8eb2b32dd08a84437b19
vedantadhau820-alt <vedantadhau820@gmail.com>
2026-01-10 17:22:23 +0530

    Create cards.js

---
+++ cards.js
@@ -0,0 +1,155 @@
+const cardCatalog = [
+
+
+{
+  id: "blackbeard",
+  title: "Marshall D. Teach",
+  quote: "People’s dreams never end!",
+  grade: "SS",
+  cost: 186,
+  image: "Images/SS1.jpg"
+},
+{
+  id: "imu",
+  title: "Imu",
+  quote: "Erase them from history.",
+  grade: "SS",
+  cost: 189,
+  image: "Images/SS2.jpg"
+},
+{
+  id: "doflamingo",
+  title: "Donquixote Doflamingo",
+  quote: "Whoever wins becomes justice.",
+  grade: "SS",
+  cost: 155,
+  image: "Images/SS3.jpg"
+},
+{
+  id: "kaido",
+  title: "Kaido",
+  quote: "Death completes a person.",
+  grade: "SS",
+  cost: 160,
+  image: "Images/SS4.jpg"
+},
+{
+  id: "roger",
+  title: "Gol D. Roger",
+  quote: "My treasure? You can have it.",
+  grade: "SS",
+  cost: 201,
+  image: "Images/SS5.jpg"
+},
+{
+  id: "loki",
+  title: "Loki",
+  quote: "Strength decides everything.",
+  grade: "SS",
+  cost: 164,
+  image: "Images/SS6.jpg"
+},
+{
+  id: "xebec",
+  title: "Rocks D. Xebec",
+  quote: "The world itself is my enemy.",
+  grade: "SS",
+  cost: 199,
+  image: "Images/SS7.jpg"
+},
+{
+  id: "rayleigh",
+  title: "Silvers Rayleigh",
+  quote: "Living isn’t about following rules.",
+  grade: "SS",
+  cost: 175,
+  image: "Images/SS8.jpg"
+},
+{
+  id: "gaban",
+  title: "Scopper Gaban",
+  quote: "The Pirate King’s crew fears nothing.",
+  grade: "SS",
+  cost: 169,
+  image: "Images/SS9.jpg"
+},
+{
+  id: "whitebeard",
+  title: "Edward Newgate",
+  quote: "One Piece… is real!",
+  grade: "SS",
+  cost: 176,
+  image: "Images/SS10.jpg"
+},
+{
+  id: "aizen",
+  title: "Sosuke Aizen",
+  quote: "Admiration is the furthest thing from understanding.",
+  grade: "SS",
+  cost: 146,
+  image: "Images/SS11.jpg"
+},
+{
+  id: "zeno",
+  title: "Zeno",
+  quote: "Erase!",
+  grade: "SS",
+  cost: 156,
+  image: "Images/SS12.jpg"
+},
+{
+  id: "denji",
+  title: "Denji",
+  quote: "I just want a normal life.",
+  grade: "SS",
+  cost: 150,
+  image: "Images/SS13.jpg"
+},
+{ id: "luffy", 
+title: "Monkey D. Luffy", 
+quote: "I’m gonna be King of the Pirates!", 
+grade: "SS", cost: 156, 
+image: "Images/SS14.jpg" },
+  {
+    id: "shanks",
+    title: "Shanks",
+    quote: "It’s okay to cry… but you have to move on.",
+    grade: "SS",
+    cost: 159,
+    image: "Images/SS15.jpg"
+  },
+    
+  {
+    id: "garp",
+    title: "Monkey D. Garp",
+    quote: "Training is the only thing that never betrays you.",
+    grade: "S",
+    cost: 104,
+    image: "Images/S1.jpg"
+  },
+  {
+    id: "dragon",
+    title: "Monkey D. Dragon",
+    quote: "The world is waiting for our answer.",
+    grade: "S",
+    cost: 102,
+    image: "Images/S2.jpg"
+  },
+  {
+    id: "zoro",
+    title: "Roronoa Zoro",
+    quote: "If I can’t even protect my captain’s dream, then whatever ambition I have is nothing but talk.",
+    grade: "S",
+    cost: 111,
+    image: "Images/S3.jpg"
+  },
+  {
+    id: "sanji",
+    title: "Sanji",
+    quote: "Cooking is a gift from the gods.",
+    grade: "S",
+    cost: 103,
+    image: "Images/S4.jpg"
+  },
+
+  
